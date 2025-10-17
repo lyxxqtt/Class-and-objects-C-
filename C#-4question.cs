f5552c04c1589dfab1f39abe31845c8aee045937
Calyx James <183707182+lyxxqtt@users.noreply.github.com>
2025-10-17 13:35:48 +0800

    Replace quiz program with animal display program

--- C#-4question.cs
+++ C#-4question.cs
@@ -1,42 +1,15 @@
 using System;
 
-class HelloWorld
+class Program
 {
     static void Main()
     {
-        Console.WriteLine("~~~~TAMA O MALI~~~~");
-        int score;
-        do
-        {
-            score = 0; 
+        // gumawa ako ng array na may tatlong animals
+        string[] animalsArr = { "cat", "dog", "bird" };
 
-            Console.WriteLine("1. Ang C# ba ay isang programming language?");
-            string firstQues = Console.ReadLine();
-
-            Console.WriteLine("2. For Backend lang ba ang C#? ");
-            string secondtQues = Console.ReadLine();
-
-            Console.WriteLine("3. Puwede ba ito gamitin for backend sa webdevelopment? ");
-            string thirdtQues = Console.ReadLine();
-
-            Console.WriteLine("4. Csharp ba ang meaning C#? ");
-            string fourthtQues = Console.ReadLine();
-
-            if (firstQues == "tama") score++;
-            if (secondtQues == "mali") score++;
-            if (thirdtQues == "tama") score++;
-            if (fourthtQues == "tama") score++;
-
-            Console.WriteLine("\nAng score mo ay: " + score + "/4");
-
-            if (score < 2)
-            {
-                Console.Clear();
-                Console.WriteLine("Bagsak ka, retake ka:\n");
-            }
-
-        } while (score < 3);
-
-        Console.WriteLine("Congrats ANG GALING MO!");
+        // gumamit ako ng for loop para ma-display lahat ng animals
+        for (int i = 0; i < animalsArr.Length; i++)
+            // ipi-print yung bawat animal
+            Console.WriteLine(animalsArr[i]);
     }
 }
